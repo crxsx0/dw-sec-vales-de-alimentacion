676766f6eb89d73037fab2cd8c9efb6c01cf37f5
crxsx0 <crxsx.0@gmail.com>
2024-11-24 15:09:31 -0300

    Agregar validación de permisos para la creación, edición y eliminación de vales: restringir acceso a administradores.

--- backend/routes/vales.js
+++ backend/routes/vales.js
@@ -15,16 +15,25 @@ router.get('/:id', (req, res) => {
 
 // Ruta para crear un vale
 router.post('/', (req, res) => {
+    if (req.rolAuth !== 'administrador') {
+        return res.status(401).json({ message: 'No tienes permisos para realizar esta acción.' });
+    }
     crearVale(req, res);
 });
 
 // Ruta para editar un vale
 router.put('/:id', (req, res) => {
+    if (req.rolAuth !== 'administrador') {
+        return res.status(401).json({ message: 'No tienes permisos para realizar esta acción.' });
+    }
     editarVale(req, res);
 });
 
 // Ruta para eliminar un vale
 router.delete('/:id', (req, res) => {
+    if (req.rolAuth !== 'administrador') {
+        return res.status(401).json({ message: 'No tienes permisos para realizar esta acción.' });
+    }
     eliminarVale(req, res);
 });
 
